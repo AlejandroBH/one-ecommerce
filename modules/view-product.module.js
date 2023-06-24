b9e1cd87c436c453c0166b900ab37c3da18f5ffc
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-06-24 19:51:25 -0400

    view product module

--- modules/view-product.module.js
+++ modules/view-product.module.js
@@ -1,8 +1,9 @@
 "use strict";
 
+import { createCard } from "../controllers/create-card.controller.js";
 import { getParamByUrl } from "../controllers/get-param.controller.js";
 import { titleApp, viewProductImage, viewProductInfoDescription, viewProductInfoPrice, viewProductInfoTitle } from "../script/app.js";
-import { getProductById } from "../services/product.service.js";
+import { getProductById, getProducts } from "../services/product.service.js";
 
 const productId = getParamByUrl("id");
 
@@ -19,6 +20,31 @@ if (location.search === "") {
       viewProductInfoTitle.textContent = data.name;
       viewProductInfoPrice.textContent = `$ ${data.price}`;
       viewProductInfoDescription.textContent = data.description;
+      getProducts().then(product => {
+        const arrProduct = [];
+        let limit = 0;
+        
+        for (let item of product) {
+          if (item.category == data.category) {
+            if (item.id == productId){
+              continue;
+            }
+            arrProduct.push(item);
+          }
+        }
+        if (arrProduct.length >= 6) {
+          limit = 6
+        } else {
+          limit = arrProduct.length;
+        }
+        for (let i = 0; i < limit; i++) {
+          createCard(arrProduct[i]);
+        }
+        const prodcutCard = document.querySelectorAll(".products__cards__item");
+        for (let item of prodcutCard) {
+          item.classList.add("item-limit");
+        }
+      });
     }
   });
 }
