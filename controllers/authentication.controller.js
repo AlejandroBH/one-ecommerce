9e49a8ff62e978f32a882437a50ab4e4f8d99fc7
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-06-26 16:40:47 -0400

    authentication

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -5,16 +5,17 @@ import { getUserById, getUsers } from "../services/user.service.js";
 export const validateAuthentication = (email, password) => {
   getUsers().then(users => {
     for (let user of users) {
-      if (user.email === email){
-        if (user.email === email) {
-          getUserById(user.id).then(user => {
-            if (user.password === password) {
-              // TODO (generar alerta si contraseña es correcta y guardar sesion en local storage)
-            } else {
-              // TODO (generar alerta si contraseña es incorrecta)
-            }
-          })
-        }
+      if (user.email === email) {
+        console.log(`cuenta ${user.name} existe`);
+        getUserById(user.id).then(user => {
+          if (user.password === password) {
+            // TODO (generar alerta si contraseña es correcta y guardar sesion en local storage)
+            console.log('clave correcta');
+          } else {
+            // TODO (generar alerta si contraseña es incorrecta)
+            console.log('clave incorrecta');
+          }
+        })
       } else {
         // TODO (generar alerta si cuenta no existe)
       }
