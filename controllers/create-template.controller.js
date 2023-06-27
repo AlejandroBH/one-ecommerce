2d40255078d688c7ea59a9feb42233b0a099af95
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-06-26 21:11:54 -0400

    fix link card testing

--- controllers/create-template.controller.js
+++ controllers/create-template.controller.js
@@ -8,7 +8,7 @@ export const createCard = (item) => {
     <img class="products__cards__item__image" src="${item.image}" alt="${item.name}">
     <h5 class="products__cards__item__name">${item.name}</h5>
     <h5 class="products__cards__item__price">$ ${item.price}</h5>
-    <a href="../pages/view-product.html?id=${item.id}" class="products__cards__item__button">Ver producto</a>
+    <a href="/pages/view-product.html?id=${item.id}" class="products__cards__item__button">Ver producto</a>
   </div>
   `;
   productsCards.innerHTML += template;
