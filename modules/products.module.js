a71bdfcafabbc65700283f813be974d41295bdda
Alejandro Barrera <alejandro91dark@hotmail.es>
2023-07-08 18:46:45 -0400

    loading in product

--- modules/products.module.js
+++ modules/products.module.js
@@ -2,12 +2,17 @@
 
 import { createCard } from "../controllers/create-template.controller.js";
 import { getParamByUrl } from "../controllers/get-param.controller.js";
-import { titleApp } from "../script/app.js";
+import { insertLoading } from "../controllers/loading.controller.js";
+import { extraPage, titleApp } from "../script/app.js";
 import { getCategoryById } from "../services/category.service.js";
 import { getProducts } from "../services/product.service.js";
 
 const categoryId = getParamByUrl("category");
+const header = document.querySelector(".products__header");
 const titleCategory = document.querySelector(".products__header__title");
+
+header.style.display="none";
+insertLoading(true, extraPage);
 
 if (location.search === ""){
   location.href = "../pages/error.html";
@@ -16,8 +21,10 @@ if (location.search === ""){
     if (category.name === undefined){
       location.href = "../pages/error.html";
     } else {
+      header.style.display="flex";
+      insertLoading(false, extraPage);
       document.title = `${category.name} - ${titleApp}`;
-      titleCategory.innerHTML += category.name;
+      titleCategory.innerHTML += `Categoria ${category.name}`;
       getProducts().then(product => {
         product.forEach((item) => {
           if(item.category == categoryId){
